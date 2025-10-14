773fd772ac49483fc02c614cce0c8afa19675263
Sarzn <3537124715@qq.com>
2025-10-15 05:36:36 +0800

    Site updated: 2025-10-15 05:36:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/15/hello-world/"];function toRandomPost(){
+var posts=["2025/10/15/这是一次测试/","2025/10/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
